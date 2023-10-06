29ab099ecdb04a30d90d40764cf8003b7567c1ba
florence-manu <florencehwesumanu@gmail.com>
2023-10-06 13:11:45 +0000

    removed unsed imports

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Header } from "../../components/header";
 import { Outlet } from "react-router-dom";
